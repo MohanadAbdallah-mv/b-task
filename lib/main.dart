02cf1a8c54f244cf6297cd49d444ece9aebee4a7
MohanadAbdallah-mv <mohanad1967@yahoo.com>
2025-07-13 13:03:33 +0300

    init bot toast

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:blnk_task/providers/governorate_provider.dart';
 import 'package:blnk_task/providers/user_provider.dart';
 import 'package:blnk_task/routing/app_router.dart';
+import 'package:blnk_task/util/app_cache.dart';
 import 'package:blnk_task/util/routes.dart';
+import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+  await AppCache.instance.init();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(create: (_) => GovernorateProvider()),
@@ -38,6 +43,9 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
+      builder: BotToastInit(),
+      navigatorKey: navigatorKey,
+      navigatorObservers: [BotToastNavigatorObserver()],
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
